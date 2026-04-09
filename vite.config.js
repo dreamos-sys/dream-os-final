3a00f7898393682a99efb38850fd267967cacd5b
Master Architect <master@architect.com>
2026-04-09 22:58:30 +0700

    ISO 9001: Dream OS Versi 2.1 - Official Live Build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte({
-    onwarn: (warning, handler) => {
-      if (warning.code.startsWith('a11y-')) return;
-      handler(warning);
-    }
-  })],
-  base: '/dream-os-final/'
-});
+  base: '/dream-os-final/',
+  plugins: [svelte()],
+})
