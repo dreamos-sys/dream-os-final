f0cb9a990d63bc9fa55540cb97c1145bbc61cf74
DreamOS-Sys <dreamos@example.com>
2026-04-11 08:59:24 +0700

    Fix: remove manual index.html, correct base, remove nodeVersion

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  base: '/dream/',
+  base: '/',
   plugins: [svelte()],
   build: {
-    target: 'esnext',
-    minify: false,
-    cssMinify: false,
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      external: [] // No external modules
-    }
+    emptyOutDir: true
   }
 })
