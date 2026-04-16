9881f361dfdd3e016243a2fb23cddcda6fdc71be
DreamOS-Sys <dreamos@example.com>
2026-04-16 10:10:07 +0700

    🚀 Dream OS v2.1 - Static HTML Deploy [10:10]

--- k3-automation/lib/storage.sh
+++
@@ -1,7 +0,0 @@
-
-storage_update() {
-  local collection="$1" id="$2" updates="$3"
-  local current=$(storage_get "$collection" "$id") || return 1
-  echo "$current" | jq --argjson upd "$updates" '. + $upd' | \
-    storage_save "$collection" "$id"
-}
