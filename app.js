e4132131627b8a765c7b56edec0c238062c384ea
DreamOS-Sys <dreamos@example.com>
2026-04-25 13:27:26 +0700

    feat: fix: Sovereign deploy v3.0 2026-04-25 13:27

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-import{getPath}from'./scripts/helpers/router.js';import{renderGrid}from'./scripts/helpers/ui.js';
-window.bukaModul=async i=>(await import(getPath(i))).default();
-document.addEventListener('DOMContentLoaded',()=>{
-window.renderGrid=renderGrid;const d=document,g=i=>d.getElementById(i),f=g('login-form');
-if(f)f.onsubmit=e=>{e.preventDefault();g('login-screen').style.display='none';
-g('dashboard-screen').classList.add('active');renderGrid()}});
+import{getPath}from'./scripts/helpers/router.js';
+import{renderGrid}from'./scripts/helpers/ui.js';
+const d=document,g=i=>d.getElementById(i);
+window.bukaModul=async i=>{
+  const c=g('module-container');
+  c.innerHTML='<div id="module-viewport" class="p-4"></div>'; // BANGUN KAMARNYA DULU!
+  try{
+    const m=await import(getPath(i));
+    await m.default();
+  }catch(e){
+    c.innerHTML='<p class="text-red-500">Kabel Putus!</p>';
+    console.error(e);
+  }
+};
+d.addEventListener('DOMContentLoaded',()=>{
+  window.renderGrid=renderGrid;
+  const f=g('login-form');
+  if(f)f.onsubmit=e=>{
+    e.preventDefault();
+    g('login-screen').style.display='none';
+    g('dashboard-screen').classList.add('active');
+    renderGrid();
+  }
+});
