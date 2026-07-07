30a45a918c84358e723aa583965ce0838e2255d3
Dream Team <dream@dev.os>
2026-07-07 21:18:12 +0700

    feat: realtime notif, PWA offline, tombol sync gudang ke cloud

--- sw.js
+++ sw.js
@@ -10,14 +10,14 @@ const ASSETS = [
   '/modules/profile.html',
   '/modules/k3.html',
   '/modules/booking.html',
-  '/modules/maintenance.html'
+  '/modules/maintenance.html',
+  '/modules/asset.html',
+  '/modules/stok.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
