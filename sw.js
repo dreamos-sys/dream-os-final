e39bedcd53d9ea5f68464da7114110fda61bc6b0
Dream Team <dream@dev.os>
2026-07-07 21:08:10 +0700

    feat: service worker untuk offline support (PWA)

--- sw.js
+++ sw.js
@@ -1,16 +1,45 @@
-const CACHE_NAME = 'dream-os-v13-pro';
-const ASSETS = ['/dream-os-final/', '/dream-os-final/index.html', '/dream-os-final/assets/icon-192.png'];
+const CACHE_NAME = 'dreamos-v1.2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/logo-sultan.png',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png',
+  '/modules/commandcenter.html',
+  '/modules/profile.html',
+  '/modules/k3.html',
+  '/modules/booking.html',
+  '/modules/maintenance.html'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
